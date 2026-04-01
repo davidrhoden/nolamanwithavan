c98a10e136d6fca374b493cee84a10d9d8bfb132
David Rhoden <david@davidrhoden.com>
2026-04-01 17:01:45 -0500

    better description for SEO, I think.

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ export default {
 	title: "NOLA Man With A Van....but it's a pickup truck.",
 	url: "https://nolamanwithvan.com/",
 	language: "en",
-	description: "Moving something a little too big to fit in your car? Wish you had a pickup truck? Well, I have one. Call me at (504) 507-0254.",
+	description: "Furniture moves, dump runs, yard work, hardware store pickups. Call or text me at (504) 507-0254.",
 	author: {
 		name: "Happening Interactive Web Design and Development",
 		email: "hello@happeninginteractive.com",
